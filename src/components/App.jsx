fd78ca1b14b786722446851b2cc790a5cff7401e
Briso10-dev <kouambrice10@gmail.com>
2025-04-15 23:38:21 +0200

    First step

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import Button from "./button"
+
+function App() {
+  return (
+    <div className="bg-blue-400 h-screen flex justify-center items-start p-4">
+      <div className="bg-green-400 text-[2rem] w-[50%] h-[25%] text-center">
+        <Button />
+        <h1>Hello World!!!</h1>
+  </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/button.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Button(prop){
+    <button className="bg-gray-400 rounded-2xl ">
+        {props}
+    </button>
+}
