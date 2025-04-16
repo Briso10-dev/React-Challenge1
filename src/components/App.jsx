c8a8e71cf5acfdba6ef996c9a892d0e0da3c743d
Briso10-dev <kouambrice10@gmail.com>
2025-04-16 12:14:24 +0200

    Design section success

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,21 @@ import Button from "./button"
 
 function App() {
   return (
-    <div className="bg-blue-400 h-screen flex justify-center items-start p-4">
-      <div className="bg-green-400 text-[2rem] w-[50%] h-[25%] text-center">
-         <Button label="-"/> 
-  </div>
-    </div>
+    <div className="bg-blue-400 h-screen flex flex-col items-center gap-4 p-4">
+      <div className="flex justify-center items-center bg-green-400 text-[2rem] w-[25%] h-[15%] text-center">
+        <Button label="-" />
+        <p>Step : </p>
+        <Button label="+" />
+      </div>
+      <div className="flex justify-center items-center bg-red-400 text-[2rem] w-[25%] h-[15%] p-2 text-center">
+        <hr />
+        <Button label="-" />
+        <p>Count : </p>
+        <Button label="+" />
+        <hr />
+      </div>
+        <p className="text-[2rem]">days from today is Wed jul 2027</p>
+      </div>
   )
 }
 
--- src/components/button.jsx
+++ src/components/button.jsx
@@ -2,7 +2,7 @@
 
 export default function Button({ label }){
     return (
-        <button className="hover:font-bold text-[2rem] text-center text-black border-[0.1rem] border-black w-18 h-18 rounded-[0.8rem] bg-gray-400">
+        <button className="hover:font-bold text-[2rem] text-center text-black border-[0.1rem] border-black w-16 h-18 rounded-[0.8rem] bg-gray-400">
          {label}
         </button>
       );
