21598d7d99ab6848dd8db0b60238f4241cc18cd6
Briso10-dev <kouambrice10@gmail.com>
2025-04-16 12:41:20 +0200

    Partially successfully updating changing variables

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,29 @@
 import Button from "./button"
+import { useState } from "react"
 
 function App() {
+  const [counter,setCounter] = useState(0);
+
+  //function for incrementaton
+  function handleNext(){
+      setCounter((c)=>c+1)
+  }
+  function handlePrevious(){
+      setCounter((c)=>c-1)
+  }
+
   return (
     <div className="bg-blue-400 h-screen flex flex-col items-center gap-4 p-4">
       <div className="flex justify-center items-center bg-green-400 text-[2rem] w-[25%] h-[15%] text-center">
-        <Button label="-" />
-        <p>Step : </p>
-        <Button label="+" />
+        <Button label="-" click={handlePrevious}/>
+        <p>Step : {counter}  </p>
+        <Button label="+" click={handleNext}/>
       </div>
       <div className="flex justify-center items-center bg-red-400 text-[2rem] w-[25%] h-[15%] p-2 text-center">
         <hr />
-        <Button label="-" />
-        <p>Count : </p>
-        <Button label="+" />
+        <Button label="-" click={handlePrevious}/>
+        <p>Count : {counter} </p>
+        <Button label="+" click={handleNext}/>
         <hr />
       </div>
         <p className="text-[2rem]">days from today is Wed jul 2027</p>
--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,9 +1,13 @@
 
 
-export default function Button({ label }){
+export default function Button({ label,click}){
     return (
-        <button className="hover:font-bold text-[2rem] text-center text-black border-[0.1rem] border-black w-16 h-18 rounded-[0.8rem] bg-gray-400">
+        <button 
+        className="hover:font-bold text-[2rem] text-center text-black border-[0.1rem] border-black w-16 h-18 rounded-[0.8rem] bg-gray-400"
+        onClick={click}
+        >
          {label}
+         
         </button>
       );
 }
