a0bee909c82bbdc0bb0f2bd853aed4e10360ce1f
Briso10-dev <kouambrice10@gmail.com>
2025-04-16 12:01:20 +0200

    Correcting function return statement and understanding

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,7 @@ function App() {
   return (
     <div className="bg-blue-400 h-screen flex justify-center items-start p-4">
       <div className="bg-green-400 text-[2rem] w-[50%] h-[25%] text-center">
-        <Button />
-        <h1>Hello World!!!</h1>
+         <Button label="-"/> 
   </div>
     </div>
   )
--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,7 +1,9 @@
 
 
-export default function Button(prop){
-    <button className="bg-gray-400 rounded-2xl ">
-        {props}
-    </button>
+export default function Button({ label }){
+    return (
+        <button className="hover:font-bold text-[2rem] text-center text-black border-[0.1rem] border-black w-18 h-18 rounded-[0.8rem] bg-gray-400">
+         {label}
+        </button>
+      );
 }
