950b2aba118a175ca41d309f6b06b38abb55b8b8
Briso10-dev <kouambrice10@gmail.com>
2025-04-25 13:20:04 +0200

    Width button's component modified

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -3,7 +3,7 @@
 export default function Button({ label,click}){
     return (
         <button 
-        className="hover:font-bold text-[2rem] text-center text-black border-[0.1rem] border-black w-16 h-18 rounded-[0.8rem] bg-gray-400"
+        className="hover:font-bold text-[2rem] text-center text-black border-[0.1rem] border-black w-26 h-18 rounded-[0.8rem] bg-gray-400"
         onClick={click}
         >
          {label}
